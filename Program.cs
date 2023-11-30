e4bb6835e607616924530fc260552a3d5f065d31
Daniel <dwgrossberg@gmail.com>
2023-11-30 13:34:11 -0500

    Setup basic project structure

--- Program.cs
+++ Program.cs
@@ -1,14 +1,53 @@
-﻿namespace CSVParser;
+﻿using System;
+using System.IO;
+
+namespace CSVParser;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your name?");
-        var name = Console.ReadLine();
-        var currentDate = DateTime.Now;
-        Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
-        Console.Write($"{Environment.NewLine}Press any key to exit...");
-        Console.ReadKey(true);
+        // Get csv file name from user
+        Console.WriteLine("What is the name of the file you would like to search for?");
+        var fileName = Console.ReadLine();
+        try
+        {
+            // Search project working directory for matching csv file
+            string directory = Environment.CurrentDirectory;
+            string filePath = $"{directory}/{fileName}.csv";
+            Console.Write(filePath);
+
+            // Extract emails from csv and save in a list
+            StreamReader reader = null;
+            if (File.Exists(filePath)){
+                reader = new StreamReader(File.OpenRead(filePath));
+                List<string> emails = new List<string>();
+                // Loop through all rows within csv file
+                while (!reader.EndOfStream){
+                    var line = reader.ReadLine();
+                    Console.WriteLine(line);
+                    var values = line.Split(',');
+                    // Loop through all columns in csv file
+                    foreach (var item in values){
+                        emails.Add(item);
+                        Console.WriteLine(item);
+                    }
+                    foreach (var coloumn1 in emails){
+                        //Console.WriteLine(coloumn1);
+                    }
+                }
+            } else {
+                Console.WriteLine("File doesn't exist");
+            }
+            Console.ReadLine();
+
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+        
     }
+
 }
